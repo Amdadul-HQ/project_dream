a6d86418e6eb5cf1042fa8538520e97f5d6fa311
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-08-11 12:53:04 +0600

    add: user like comment and follow api created

--- apps/backend/src/main/user/user.controller.ts
+++ apps/backend/src/main/user/user.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Delete, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetUser, ValidateAuth } from '@project/common/jwt/jwt.decorator';
 import { LikeService } from './service/like.service';
 import { CommentService } from './service/comment.service';
 import { FollowService } from './service/follow.service';
+import { CreateCommentDto } from './dto/createComment.dto';
+import { CreateFollowDto } from './dto/createFollowee.dto';
 
 @ApiTags('User ---')
 @Controller('user/post')
@@ -16,6 +18,7 @@ export class UserController {
     private readonly followService: FollowService,
   ) {}
 
+  @ApiTags('User Like Post---')
   @Post('like/:postId')
   @ApiOperation({ summary: 'Post A Like' })
   async postLike(
@@ -26,6 +29,7 @@ export class UserController {
   }
 
   // Unlike a post
+  @ApiTags('User Like Post---')
   @Delete('unlike/:postId')
   @ApiOperation({ summary: 'Unlike a post' })
   async unlikePost(
@@ -33,5 +37,46 @@ export class UserController {
     @GetUser('userId') userId: string,
   ) {
     return await this.likeService.unlikePost(postId, userId);
+  }
+
+  // Post Comment
+  @ApiTags('User Comment Post---')
+  @Post()
+  @ApiOperation({ summary: 'Create a new comment on a post' })
+  async createComment(
+    @Body() createCommentDto: CreateCommentDto,
+    @GetUser('userId') userId: string,
+  ) {
+    return this.commentService.createComment(createCommentDto, userId);
+  }
+
+  @ApiTags('User Comment Post---')
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete a comment' })
+  // @UseGuards(AuthGuard('jwt'))
+  async deleteComment(@Param('id') id: string) {
+    const userId = 'some-authenticated-user-id';
+    return this.commentService.deleteComment(id, userId);
+  }
+
+  //   User Follow
+  @ApiTags('User Follow Writer---')
+  @Post('follow')
+  @ApiOperation({ summary: 'Follow a user' })
+  // @UseGuards(AuthGuard('jwt'))
+  async followUser(@Body() createFollowDto: CreateFollowDto) {
+    const followerId = 'some-authenticated-user-id';
+    return this.followService.followUser(
+      followerId,
+      createFollowDto.followeeId,
+    );
+  }
+  @ApiTags('User Follow Writer---')
+  @Delete('unfollow/:followeeId')
+  @ApiOperation({ summary: 'Unfollow a user' })
+  // @UseGuards(AuthGuard('jwt'))
+  async unfollowUser(@Param('followeeId') followeeId: string) {
+    const followerId = 'some-authenticated-user-id';
+    return this.followService.unfollowUser(followerId, followeeId);
   }
 }
