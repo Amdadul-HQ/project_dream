2d30dc2cdd19ee89d1b406d880ecf20331a6fd47
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-08-11 14:33:39 +0600

    private chat system implement

--- apps/backend/src/main/main.module.ts
+++ apps/backend/src/main/main.module.ts
@@ -5,6 +5,7 @@ import { PostCategoryModule } from './admin/post-category/post-category.module';
 import { PostModule } from './admin/post/post.module';
 import { UserModule } from './user/user.module';
 import { DashboardModule } from './admin/dashboard/dashboard.module';
+import { PrivateChatModule } from './private-chat/private-chat.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { DashboardModule } from './admin/dashboard/dashboard.module';
     PostModule,
     UserModule,
     DashboardModule,
+    PrivateChatModule,
   ],
   controllers: [],
   providers: [],
