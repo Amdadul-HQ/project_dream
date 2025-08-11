643b07297319c7b655aa3b9d7acbecb410cda501
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-08-11 12:44:48 +0600

    like commnet service done

--- apps/backend/src/main/user/user.controller.ts
+++ apps/backend/src/main/user/user.controller.ts
@@ -1,6 +1,6 @@
-import { Controller } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { ValidateAuth } from '@project/common/jwt/jwt.decorator';
+import { Controller, Delete, Param, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { GetUser, ValidateAuth } from '@project/common/jwt/jwt.decorator';
 import { LikeService } from './service/like.service';
 import { CommentService } from './service/comment.service';
 import { FollowService } from './service/follow.service';
@@ -15,4 +15,23 @@ export class UserController {
     private readonly commentService: CommentService,
     private readonly followService: FollowService,
   ) {}
+
+  @Post('like/:postId')
+  @ApiOperation({ summary: 'Post A Like' })
+  async postLike(
+    @Param('postId') postId: string,
+    @GetUser('userId') userId: string,
+  ) {
+    return this.likeService.likePost(postId, userId);
+  }
+
+  // Unlike a post
+  @Delete('unlike/:postId')
+  @ApiOperation({ summary: 'Unlike a post' })
+  async unlikePost(
+    @Param('postId') postId: string,
+    @GetUser('userId') userId: string,
+  ) {
+    return await this.likeService.unlikePost(postId, userId);
+  }
 }
