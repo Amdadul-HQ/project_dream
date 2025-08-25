16c4ee79c4fe687dca2a997d1d847a99dd3810af
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-08-25 21:55:28 +0600

    fix: component formet

---
+++ apps/frontend/src/app/(main)/for-you/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useState } from "react";
+import { Demodata } from "@/assets/Demodata";
+import TabComponent from "@/components/home-components/PostsCollection/TabComponent";
+import GridPostCard from "@/components/home-components/PostsCollection/GridPostCard";
+import PostCard from "@/components/home-components/PostsCollection/PostCard";
+
+const ForYou = () => {
+    const [gridMode, setGridMode] = useState(false);
+    const [active, setActive] = useState(0);
+    const desktopTabs = ["Recent", "Popular", "Following", "Liked", "Top Rated", "Bookmarked"]
+    return (
+        <div className="bg-white rounded-lg shadow-md pb-5">
+
+            <div className="w-full relative sm:hidden block">
+                <TabComponent
+                    tabs={desktopTabs}
+                    activeTab={active}
+                    onTabChange={setActive}
+                    gridMode={gridMode}
+                    setGridMode={setGridMode}
+                />
+            </div>
+            <div
+                className={`grid ${gridMode ? "sm:grid-cols-3 grid-cols-1 gap-5" : "grid-cols-1 gap-4"
+                    } px-5 w-full`}
+            >
+                {Demodata.map((item, index) =>
+                    gridMode ? (
+                        <GridPostCard key={index} item={item} />
+                    ) : (
+                        <PostCard key={index} item={item} />
+                    )
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default ForYou;
--- apps/frontend/src/components/home-components/PostsCollection/TabComponent.tsx
+++ apps/frontend/src/components/home-components/PostsCollection/TabComponent.tsx
@@ -19,23 +19,23 @@ interface TabComponentProps {
   setGridMode: (mode: boolean) => void;
 }
 
-export default function TabComponent({ gridMode, setGridMode,tabs}: TabComponentProps) {
+export default function TabComponent({ gridMode, setGridMode, tabs }: TabComponentProps) {
   return (
     <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 px-4 sm:px-6 lg:px-8 py-3 sm:py-5">
       {/* Tabs using shadcn */}
       <Tabs defaultValue={tabs[0]} className="w-full">
-               <ScrollArea>
-        <div className="w-full relative h-10">
-          <TabsList className="flex absolute h-10">
-            {tabs.map((tab,id) => (
-              <TabsTrigger key={id} value={tab}>
-                {tab}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-        </div>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
+        <ScrollArea>
+          <div className="w-full relative h-10">
+            <TabsList className="flex absolute h-10">
+              {tabs.map((tab, id) => (
+                <TabsTrigger key={id} value={tab}>
+                  {tab}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
 
         {tabs.map((tab) => (
           <TabsContent key={tab} value={tab}>
@@ -49,17 +49,15 @@ export default function TabComponent({ gridMode, setGridMode,tabs}: TabComponent
       <div className="flex gap-2 shrink-0">
         <button
           onClick={() => setGridMode(false)}
-          className={`${
-            gridMode ? "bg-white" : "bg-hover"
-          } w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-md border border-gray-200 text-secondary hover:bg-hover transition-colors`}
+          className={`${gridMode ? "bg-white" : "bg-hover"
+            } w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-md border border-gray-200 text-secondary hover:bg-hover transition-colors`}
         >
           <CiGrid2H className="font-semibold text-lg sm:text-xl" />
         </button>
         <button
           onClick={() => setGridMode(true)}
-          className={`${
-            gridMode ? "bg-hover" : "bg-white"
-          } w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-md border border-gray-200 text-secondary hover:bg-hover transition-colors`}
+          className={`${gridMode ? "bg-hover" : "bg-white"
+            } w-7 h-7 sm:w-8 sm:h-8 flex items-center justify-center rounded-md border border-gray-200 text-secondary hover:bg-hover transition-colors`}
         >
           <MdCheckBoxOutlineBlank className="font-semibold text-lg sm:text-xl" />
         </button>
