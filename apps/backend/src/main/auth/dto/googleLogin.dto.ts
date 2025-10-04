c4712c30ed07ce3a1a357ec119556a0fe81323ae
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-10-04 15:02:23 +0600

    chore: update project structure for better organization

--- apps/backend/src/main/auth/dto/googleLogin.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
-
-export class GoogleLoginDto {
-   
-    @ApiProperty({example: 'asdfsag-fgasdgaaf-gas', description: 'User code'})
-    @IsString()
-    code: string;
-}
--- apps/backend/test-path.ts
+++
@@ -1,4 +0,0 @@
-// Simple test file
-console.log('Test path file is working!');
-console.log('Current directory:', __dirname);
-console.log('File name:', __filename);
